921a9d1e796504d39f742969febe2b67e3c0421e
Kyle Lemire <hello@klemire.com>
2021-03-19 16:59:23 -0500

    adjusted colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import "normalize.css";
-import styles from "../styles/App.module.scss";
+import "../index.css";
 import AndouGatchi from "./AndouGatchi";
-import ChromaKey from "./ChromaKey";
 import { GatchiContextProvider } from "../contexts/GatchiContext";
 
 function App() {
-  const [bgColor, setBgColor] = useState("#FFFFFF");
   return (
     <GatchiContextProvider>
-      <div style={{ backgroundColor: bgColor }} className={styles.App}>
-        <AndouGatchi />
-        <ChromaKey bgColor={bgColor} setBgColor={setBgColor} />
-      </div>
+      <AndouGatchi />
     </GatchiContextProvider>
   );
 }
